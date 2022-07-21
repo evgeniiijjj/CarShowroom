181ab4df35d4654bf6dc22b5c68fa11883956f96
evgeniiijjj <100117060+evgeniiijjj@users.noreply.github.com>
2022-07-21 03:05:58 +0300

    Update CarSupplier.java

--- src/main/java/CarSupplier.java
+++ src/main/java/CarSupplier.java
@@ -1,23 +1,23 @@
 public class CarSupplier implements Runnable {
     private final CarShowroom showroom;
     private final String brand;
-    private final int numCars;
+    private final int carsNum;
     private final int deliveryTime;
 
     public CarSupplier(CarShowroom showroom, String brand) {
         this.showroom = showroom;
         this.brand = brand;
-        this.numCars = showroom.getNumCars();
+        this.carsNum = showroom.getCarsNum();
         this.deliveryTime = showroom.getDeliveryTime();
     }
 
     @Override
     public void run() {
         String name = Thread.currentThread().getName();
-        System.out.println("Поставщик " + name + " заключил контракт на поставку " + numCars + " автомобилей марки " + brand);
+        System.out.println("Поставщик " + name + " заключил контракт на поставку " + carsNum + " автомобилей марки " + brand);
         System.out.println("Поставщик " + name + " приступил к выполнению контракта");
         try {
-            for (int i = 0; i < numCars; i++) {
+            for (int i = 0; i < carsNum; i++) {
                 Thread.sleep(deliveryTime);
                 showroom.putCar(new Car(brand));
                 System.out.println("Поставщик " + name + " поставил автомобиль");
